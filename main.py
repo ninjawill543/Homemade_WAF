7e62c0709fa002cc63250cf9930f756d79bd9165
ninjawill543 <lucas.hanson@ynov.com>
2024-03-28 11:52:01 +0100

    created new separate html files

--- main.py
+++ main.py
@@ -8,14 +8,42 @@ logging.basicConfig(filename="logs.txt", level=logging.DEBUG, format=f'%(asctime
 def home():
     return render_template("home.html")
 
-@app.route("/submit", methods=["POST"])
-def submit():
+@app.route("/xss")
+def xss():
+    return render_template("xss_test.html")
+
+@app.route("/sql")
+def sql():
+    return render_template("sql_test.html")
+
+@app.route("/image")
+def image():
+    return render_template("image_test.html")
+
+@app.route("/submit_sql", methods=["POST"])
+def submit_sql():
     if request.method == "POST":
         username = request.form['username']
         password = request.form['password']
+        print(f"Username: {username} Password: {password}")
+        app.logger.info(f"Username: {username} ; Password: {password};")
+    return "Data received"
+
+@app.route("/submit_xss", methods=["POST"])
+def submit_xss():
+    if request.method == "POST":
+        input = request.form['input']
+        print(f"Input: {input}")
+        app.logger.info(f"XSS input: {input};")
+    return "Data received"
+
+@app.route("/submit_image", methods=["POST"])
+def submit_image():
+    if request.method == "POST":
         file = request.form['file']
-        app.logger.info(f"Username: {username} ; Password: {password} ; File: {file}")
+        print(f"File: {file}")
+        app.logger.info(f"File: {file};")
     return "Data received"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
