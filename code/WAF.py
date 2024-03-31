8192d6a84441f83df0ab2f46f10449d87ebd2b31
ninjawill543 <lucas.hanson@ynov.com>
2024-03-31 18:34:32 +0200

    cleaning up

--- code/WAF.py
+++ code/WAF.py
@@ -31,89 +31,94 @@ def index():
         response = Response(resp.content, resp.status_code, headers)
         return response
 
-@app.route("/xss", methods=['GET','POST'])
-def xss():
-    # input_data = request.form['input']
-    # return f"XSS Input: {input_data}"
+
+@app.route("/<path:path>",methods=["GET","POST"])
+def proxy(path):
     global SITE_NAME
-    if request.method=="GET":
-        resp = requests.get(f"{SITE_NAME}/xss")
+    if request.method=="GET": 
+        resp = requests.get(f"{SITE_NAME}/{path}")
         excluded_headers = ["content-encoding", "content-length", "transfer-encoding", "connection"]
         headers = [(name, value) for (name, value) in  resp.raw.headers.items() if name.lower() not in excluded_headers]
         response = Response(resp.content, resp.status_code, headers)
         return response
     elif request.method=="POST":
-        resp = requests.post(f"{SITE_NAME}/xss",data=request.form)
+        resp = requests.post(f"{SITE_NAME}/{path}",data=request.form)
         excluded_headers = ["content-encoding", "content-length", "transfer-encoding", "connection"]
         headers = [(name, value) for (name, value) in resp.raw.headers.items() if name.lower() not in excluded_headers]
         response = Response(resp.content, resp.status_code, headers)
         return response
 
-@app.route("/delete_comment", methods=['POST'])
-def delete_comment():
-    global SITE_NAME
-    if request.method=="POST":
-        resp = requests.post(f"{SITE_NAME}/delete_comment",data=request.form)
-        excluded_headers = ["content-encoding", "content-length", "transfer-encoding", "connection"]
-        headers = [(name, value) for (name, value) in resp.raw.headers.items() if name.lower() not in excluded_headers]
-        response = Response(resp.content, resp.status_code, headers)
-        return response
-
-@app.route("/sql", methods=['GET','POST'])
-def sql():
-    global SITE_NAME
-    if request.method=="GET":
-        resp = requests.get(f"{SITE_NAME}/sql")
-        excluded_headers = ["content-encoding", "content-length", "transfer-encoding", "connection"]
-        headers = [(name, value) for (name, value) in  resp.raw.headers.items() if name.lower() not in excluded_headers]
-        response = Response(resp.content, resp.status_code, headers)
-        return response
-    elif request.method=="POST":
-        resp = requests.post(f"{SITE_NAME}/sql",data=request.form)
-        excluded_headers = ["content-encoding", "content-length", "transfer-encoding", "connection"]
-        headers = [(name, value) for (name, value) in resp.raw.headers.items() if name.lower() not in excluded_headers]
-        response = Response(resp.content, resp.status_code, headers)
-        return response
-    # username = request.form['username']
-    # password = request.form['password']
-    # return f"Username: {username}, Password: {password}"
-
-
-@app.route("/image", methods=['GET','POST'])
-def image():
-    global SITE_NAME
-    if request.method=="GET":
-        resp = requests.get(f"{SITE_NAME}/image")
-        excluded_headers = ["content-encoding", "content-length", "transfer-encoding", "connection"]
-        headers = [(name, value) for (name, value) in  resp.raw.headers.items() if name.lower() not in excluded_headers]
-        response = Response(resp.content, resp.status_code, headers)
-        return response
-    elif request.method=="POST":
-        resp = requests.post(f"{SITE_NAME}/image",data=request.form)
-        excluded_headers = ["content-encoding", "content-length", "transfer-encoding", "connection"]
-        headers = [(name, value) for (name, value) in resp.raw.headers.items() if name.lower() not in excluded_headers]
-        response = Response(resp.content, resp.status_code, headers)
-        return response
-    # file_data = request.form['file']
-    # return f"File: {file_data}"
-
-# @app.route("/<path:path>",methods=["GET","POST"])
-# def proxy(path):
+# @app.route("/xss", methods=['GET','POST'])
+# def xss():
+#     # input_data = request.form['input']
+#     # return f"XSS Input: {input_data}"
 #     global SITE_NAME
-#     if request.method=="GET": 
-#         resp = requests.get(f"{SITE_NAME}/{path}")
+#     if request.method=="GET":
+#         resp = requests.get(f"{SITE_NAME}/xss")
 #         excluded_headers = ["content-encoding", "content-length", "transfer-encoding", "connection"]
 #         headers = [(name, value) for (name, value) in  resp.raw.headers.items() if name.lower() not in excluded_headers]
 #         response = Response(resp.content, resp.status_code, headers)
 #         return response
 #     elif request.method=="POST":
-#         resp = requests.post(f"{SITE_NAME}/{path}",data=request.form)
+#         resp = requests.post(f"{SITE_NAME}/xss",data=request.form)
+#         excluded_headers = ["content-encoding", "content-length", "transfer-encoding", "connection"]
+#         headers = [(name, value) for (name, value) in resp.raw.headers.items() if name.lower() not in excluded_headers]
+#         response = Response(resp.content, resp.status_code, headers)
+#         return response
+
+# @app.route("/delete_comment", methods=['POST'])
+# def delete_comment():
+#     global SITE_NAME
+#     if request.method=="POST":
+#         resp = requests.post(f"{SITE_NAME}/delete_comment",data=request.form)
+#         excluded_headers = ["content-encoding", "content-length", "transfer-encoding", "connection"]
+#         headers = [(name, value) for (name, value) in resp.raw.headers.items() if name.lower() not in excluded_headers]
+#         response = Response(resp.content, resp.status_code, headers)
+#         return response
+
+# @app.route("/sql", methods=['GET','POST'])
+# def sql():
+#     global SITE_NAME
+#     if request.method=="GET":
+#         resp = requests.get(f"{SITE_NAME}/sql")
+#         excluded_headers = ["content-encoding", "content-length", "transfer-encoding", "connection"]
+#         headers = [(name, value) for (name, value) in  resp.raw.headers.items() if name.lower() not in excluded_headers]
+#         response = Response(resp.content, resp.status_code, headers)
+#         return response
+#     elif request.method=="POST":
+#         resp = requests.post(f"{SITE_NAME}/sql",data=request.form)
+#         excluded_headers = ["content-encoding", "content-length", "transfer-encoding", "connection"]
+#         headers = [(name, value) for (name, value) in resp.raw.headers.items() if name.lower() not in excluded_headers]
+#         response = Response(resp.content, resp.status_code, headers)
+#         return response
+#     # username = request.form['username']
+#     # password = request.form['password']
+#     # return f"Username: {username}, Password: {password}"
+
+
+# @app.route("/image", methods=['GET','POST'])
+# def image():
+#     global SITE_NAME
+#     if request.method=="GET":
+#         resp = requests.get(f"{SITE_NAME}/image")
+#         excluded_headers = ["content-encoding", "content-length", "transfer-encoding", "connection"]
+#         headers = [(name, value) for (name, value) in  resp.raw.headers.items() if name.lower() not in excluded_headers]
+#         response = Response(resp.content, resp.status_code, headers)
+#         return response
+#     elif request.method=="POST":
+#         resp = requests.post(f"{SITE_NAME}/image",data=request.form)
 #         excluded_headers = ["content-encoding", "content-length", "transfer-encoding", "connection"]
 #         headers = [(name, value) for (name, value) in resp.raw.headers.items() if name.lower() not in excluded_headers]
 #         response = Response(resp.content, resp.status_code, headers)
 #         return response
 
 
+
+
+
+
+    # file_data = request.form['file']
+    # return f"File: {file_data}"
 
 if __name__ == "__main__":
     app.run(debug = True,port=5000)
